cb5f548908ce9cb0d76b2f81dd18c7ab3f3094c3
Ethan Ooi <ethanooiyien@gmail.com>
2019-08-03 01:08:50 +0800

    Improved Logging

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var cheerio = require('cheerio');
 var axios = require('axios');
 var genius;
 var accessToken;
+const port = 8081;
 //Place your token in accesstoken.txt
 fs.readFile('accesstoken.txt',function(err,buff){
     if (err){console.log(err);}
@@ -18,11 +19,9 @@ fs.readFile('accesstoken.txt',function(err,buff){
 async function getData(artist_name) {
     try {
         return await genius.search(artist_name);
-        
-    } catch (error) {
+    } 
+    catch (error) {
         return await error;
-    } finally {
-        console.log('done');
     }
 }
 
@@ -30,18 +29,14 @@ function parseSongHTML(htmltext)
 {
     var lyrics;
     var body;
-
     body = cheerio.load(htmltext);
     lyrics = body('.lyrics').text();
-    releaseDate = body('release-date.song_info-info').text();
-   
-        return lyrics;
+    return lyrics;
 
     
 } 
 
 async function scrapeLyrics(url) {
-    console.log('Waiting for Genius Site...');
     var result = await axios.get(url);
     return parseSongHTML(result.data);
 
@@ -50,32 +45,43 @@ async function scrapeLyrics(url) {
 var instructionsNewVisitor = function (req, res) {
         var params = querystring.parse(url.parse(req.url).query); //parses params
         var urlscrape = '';
-        console.log(params);
         res.writeHead(200, {
             "Content-Type": "text/plain"
         });
 
         if ('action' in params) {
             if (params.action == "search") {
-                console.log("Waiting for Genius API...");
-                getData(params.name).then(data => {
-                    console.log("writing");
-                    urlscrape = data.hits[0].result.url;
-                    console.log(urlscrape);
-                    return scrapeLyrics(urlscrape);
-                })
-                .then(finalresult => {
-                    res.write(urlscrape+'\n \n');
-                    res.write(finalresult.substring(37));
-                    res.end();
-                })
-                .catch(e => {
-                    console.log(e);
-                });
+                console.log('');
+                try{
+                    console.log("Request Received: " + params.name)
+                    console.log("Waiting for Genius API...");
+                    getData(params.name).then(data => {
+                        urlscrape = data.hits[0].result.url;
+                        console.log("URL Found:" + urlscrape);
+                        console.log('Waiting for Genius Site...');
+                        return scrapeLyrics(urlscrape);
+                    })
+                        .then(finalresult => {
+                            res.write(urlscrape + '\n \n');
+                            res.write(finalresult.substring(37));
+                            res.end();
+                            console.log('Request Served!');
+                            console.log('');
+                        })
+                        .catch(e => {
+                            console.log(e);
+                        });
+
+                }
+                catch(err){
+                    console.log(err);
+                }
+                
+
             }
         }
 };
 
 var server = http.createServer(instructionsNewVisitor);
-server.listen(8081);
-console.log("Listening");
+server.listen(port);
+console.log("Listening on Port " + port);
